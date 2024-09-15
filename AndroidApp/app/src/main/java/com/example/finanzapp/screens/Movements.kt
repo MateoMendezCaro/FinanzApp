be0a82fb19eeaa72d826a4fc8a98bb2406684e24
Mateo <mattt7981@gmail.com>
2024-09-14 20:10:23 -0500

    Creacion de base para mockups y app

---
+++ AndroidApp/app/src/main/java/com/example/finanzapp/screens/Movements.kt
@@ -0,0 +1,29 @@
+package com.example.finanzapp.screens
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.finanzapp.Backgrounder
+import com.example.finanzapp.BottomNavigationBar
+import com.example.finanzapp.TopBar
+
+@Preview(showBackground = true)
+@Composable
+fun MovementsScreen() {
+        Scaffold(
+            topBar = { TopBar() },
+            bottomBar = { BottomNavigationBar() }
+        ) { innerPadding ->
+            Backgrounder {
+                Text(
+                    text = "contenido de la pantalla de movimientos",
+                    modifier = Modifier
+                        .padding(innerPadding)
+                        .fillMaxSize()
+                )
+            }
+        }
+    }
